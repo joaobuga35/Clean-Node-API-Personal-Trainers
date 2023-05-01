25f6441e0b553e715ad5ff3b9b91614e2bb31668
joaobuga35 <joaobuga35@gmail.com>
2023-04-30 23:16:29 -0300

    feat: ensure Login Controller return an error if no email is provided

--- src/presentation/controller/login.ts
+++ src/presentation/controller/login.ts
@@ -1,8 +1,17 @@
 export class LoginController{
 	handle(httpRequest: any): any{
-		return {
-			statusCode: 400,
-			body: new Error("Missing param: username")
-		};
+		if (!httpRequest.body.username) {
+			return {
+				statusCode: 400,
+				body: new Error("Missing param: username")
+			};
+		}
+
+		if (!httpRequest.body.email) {
+			return {
+				statusCode: 400,
+				body: new Error("Missing param: email")
+			};
+		}
 	}
 }
