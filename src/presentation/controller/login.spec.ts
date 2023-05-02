039e38f30505bd776181bce2f71aaecbee98cdc3
joaobuga35 <joaobuga35@gmail.com>
2023-05-01 23:48:41 -0300

    refactor: create personalized class errors

--- src/presentation/controller/login.spec.ts
+++ src/presentation/controller/login.spec.ts
@@ -1,4 +1,5 @@
 
+import { MissingParamError } from "../../errors";
 import { LoginController } from "./login";
 
 describe("Login Controller", () => {
@@ -13,7 +14,7 @@ describe("Login Controller", () => {
 		};
 		const httpResponse = sut.handle(httpRequest);
 		expect(httpResponse.statusCode).toBe(400);
-		expect(httpResponse.body).toEqual(new Error("Missing param: username"));
+		expect(httpResponse.body).toEqual(new MissingParamError("username"));
 	});
 
 	test("Should return 400 if missign email", () => {
@@ -27,6 +28,20 @@ describe("Login Controller", () => {
 		};
 		const httpResponse = sut.handle(httpRequest);
 		expect(httpResponse.statusCode).toBe(400);
-		expect(httpResponse.body).toEqual(new Error("Missing param: email"));
+		expect(httpResponse.body).toEqual(new MissingParamError("email"));
+	});
+
+	test("Should return 400 if missign password", () => {
+		const sut = new LoginController();
+		const httpRequest = {
+			body: {
+				username: "any_username",
+				email: "any_email@mail.com",
+				passwordConfirmation: "any_password"
+			}
+		};
+		const httpResponse = sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+		expect(httpResponse.body).toEqual(new Error("Missing param: password"));
 	});
 });
